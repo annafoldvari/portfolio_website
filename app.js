1ff3af782d2da1002411932dcfe777f9318f0192
anna_f <annafoldvari@gmx.co.uk>
2020-01-09 17:13:50 +0000

    React project added+Minor fix

--- app.js
+++ app.js
@@ -19,17 +19,27 @@ app.get('/about', (req, res) => {
 });
 
 app.get('/project/:id', (req, res) => {
-    const { id } = req.params;
-    const title = projects[id]["project_name"];
-    const description = projects[id]["description"]; 
-    const technologies = projects[id]["technologies"];
-    const live_link = projects[id]["live_link"];
-    const github_link = projects[id]["github_link"];
-    const images = projects[id]["image_urls"];
+    const id = parseInt(req.params.id);
 
-    const templateData = { id, title, description, technologies, live_link, github_link, images };
-    
-    res.render('project', templateData );
+    if (id == NaN || id >= projects.length) {
+      const error = {
+        message: "Page does not exist",
+        status: "404 - Not Found",
+        stack: ""
+      };
+      res.status(404).render('error', {error});
+    } else {
+      const title = projects[id]["project_name"];
+      const description = projects[id]["description"]; 
+      const technologies = projects[id]["technologies"];
+      const live_link = projects[id]["live_link"];
+      const github_link = projects[id]["github_link"];
+      const images = projects[id]["image_urls"];
+
+      const templateData = { id, title, description, technologies, live_link, github_link, images };
+      
+      res.render('project', templateData );
+    }
 
 });
 
